862020820b9e4a9a6a7a66349fbfe9c20b920097
Violing <ling@stoneblue.com>
2021-10-09 13:40:46 -0500

    init commit

---
+++ Variables/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            object height = 1.88;
+            object name = "Amir";
+            Console.WriteLine($"{name} is {height} metres tall.");
+
+            //int length1 = name.Length;
+            int length2 = ((string)name).Length;
+            Console.WriteLine($"{name} has {length2} characters.");
+
+            dynamic anotherName = "Ahmed";
+            int length = anotherName.Length;
+
+            var population = 66_000_000;
+            var weight = 1.88;
+            var price = 4.99M;
+            var fruit = "Apples";
+            var letter = 'Z';
+            var happy = true;
+
+            var xml1 = new XmlDocument();
+            XmlDocument xm12 = new XmlDocument();
+
+            //var file1 = File.CreateText(@".\something.txt");
+            StreamWriter file2 = File.CreateText(@".\something.txt");
+
+            XmlDocument xml3 = new();
+
+            Console.WriteLine($"default(int) = {default(int)}");
+            Console.WriteLine($"default(bool) = {default(bool)}");
+            Console.WriteLine(
+              $"default(DateTime) = {default(DateTime)}");
+            Console.WriteLine(
+              $"default(string) = {default(string)}");
+        }
+    }
+}
